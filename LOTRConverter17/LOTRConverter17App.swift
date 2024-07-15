a7a3013f3324d357e1a45cf36f7ce77ca9a383a9
Christian Phan <chrisvup@gmail.com>
2024-07-15 13:34:57 -0700

    Initial Commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Christian Phan on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
